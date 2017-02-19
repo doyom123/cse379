2f7c73fec690b41b1f3e8f356b5d9d9d6d8dc0f3
doyom123 <doom@buffalo.edu>
2017-02-19 12:03:07 -0500

    lab3 init

---
+++ lab3/Objects/lab3part1skeleton.d
@@ -0,0 +1,1 @@
+.\objects\lab3part1skeleton.o: lab3part1skeleton.s
---
+++ lab3/lab3part1skeleton.s
@@ -0,0 +1,47 @@
+	AREA	lib, CODE, READWRITE	
+	EXPORT lab3
+	EXPORT pin_connect_block_setup_for_uart0
+	
+U0LSR EQU 0x14			; UART0 Line Status Register
+
+lab3
+	STMFD SP!,{lr}	; Store register lr on stack
+    
+; Your code is placed here
+	; UART0 
+	LDR r0, =0xE000C000
+	LDRB r1, [r0, #U0LSR]
+	
+	; Read Data
+rstart
+	; Test RDR in Status Register
+	ANDS	r2, r1, #1
+	; if RDR == 0 -> rstart
+	BEQ		rstart
+	; else Read byte from receive register
+	LDRBGT	r3, [r0]
+
+	; Transmit Data
+tstart
+	; test THRE in Status Register
+	ANDS 	r2, r1, #32
+	; if THRE == 0 -> tstart
+	BEQ 	tstart
+	; else Store byte in transmit register
+	STRBGT	r3, [r]
+	
+	LDMFD sp!, {lr}
+	BX lr
+
+ 
+pin_connect_block_setup_for_uart0
+	STMFD sp!, {r0, r1, lr}
+	LDR r0, =0xE002C000  ; PINSEL0
+	LDR r1, [r0]
+	ORR r1, r1, #5
+	BIC r1, r1, #0xA
+	STR r1, [r0]
+	LDMFD sp!, {r0, r1, lr}
+	BX lr
+
+	END
